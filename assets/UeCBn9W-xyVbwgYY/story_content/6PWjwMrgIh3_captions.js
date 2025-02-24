06a949475ec38bfde3cb382e20fa805bb4a8bd10
Famxuanlinh <famxuanlinh@gmail.com>
2025-02-24 10:55:13 +0700

    Fix bootstrapper

--- assets/UeCBn9W-xyVbwgYY/story_content/6PWjwMrgIh3_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.88.32474.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:01.106%0D%0AClick%20%3Cb%3E%22NEXT%22%20%3C/b%3Eto%20continue.%0D%0A%0D%0A"}')
